4bcc6787ecadc2ce1ad112da6684bab35975b0f2
bohdanmykhalchuk <bogdan.mikhalchuk@kindgeek.com>
2022-11-17 19:12:02 +0200

    Add pessimistic locking

--- src/main/java/com/mykhalchuk/just_eat_it/repository/DishRepository.java
+++ src/main/java/com/mykhalchuk/just_eat_it/repository/DishRepository.java
@@ -2,9 +2,11 @@ package com.mykhalchuk.just_eat_it.repository;
 
 import com.mykhalchuk.just_eat_it.domain.entity.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 
 @Repository
@@ -15,4 +17,12 @@ public interface DishRepository extends JpaRepository<Dish, Long> {
             "join dish_dish_categories dc on d.id = dc.dish_id " +
             "where dc.dish_categories = :dishCategory ", nativeQuery = true)
     List<Dish> findByCategory(String dishCategory);
+
+    @Override
+    @Lock(LockModeType.PESSIMISTIC_READ)
+    List<Dish> findAll();
+
+    @Override
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    <S extends Dish> S save(S dish);
 }
